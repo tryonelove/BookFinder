a70e4b906c76c170d1212ec800d2a2923586fd0a
Varya Minkina <minkina.vj@gmail.com>
2021-03-07 15:07:35 +0300

    fix bugs

--- backend/server/app.py
+++ backend/server/app.py
@@ -5,12 +5,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('server.config.DevelopmentConfig')
 
-    from server.api.auth import auth_bp
-    from server.api.books import api_books_bp
+    from backend.server.api.auth import auth_bp
+    from backend.server.api.books import api_books_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(api_books_bp)
     
-    from server.models import db
+    from backend.server.models import db
     db.init_app(app)
     
     return app
